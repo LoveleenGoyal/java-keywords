f8a679ed6d32c88bd29c711e4ac640d5c3ce6dd8
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-11 22:15:10 +0530

    [Loveleen] ADD: Adding a banck account system

---
+++ BankAccountSystem.java
@@ -0,0 +1,49 @@
+class BankAccount {
+    // Static variable shared across all accounts
+    private static String bankName = "ABC Bank";
+    private static int totalAccounts = 0;
+
+    // Instance variables
+    private String accountHolderName;
+    private final int accountNumber; // Final variable to ensure it cannot be changed
+    private double balance;
+
+    // Constructor
+    public BankAccount(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+        totalAccounts++; // Increment total accounts
+    }
+
+    public void displayAccountDetails() {
+        if(this instanceof BankAccount) {
+            System.out.println("Bank Name: " + bankName);
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Account Holder: " + accountHolderName);
+            System.out.println("Balance: " + balance);
+        }
+    }
+
+    public static void getTotalAccounts() {
+        System.out.println("Total Bank Accounts: " + totalAccounts);
+    }
+}
+
+// Main class to test the BankAccount class
+public class BankAccountSystem {
+    public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount(1001, "Loveleen", 500000);
+        BankAccount acc2 = new BankAccount(1002, "Yagyata", 300000);
+
+        System.out.println("====Account 1====");
+        acc1.displayAccountDetails();
+        System.out.println("\n====Account 2====");
+        acc2.displayAccountDetails();
+
+        System.out.println();
+        BankAccount.getTotalAccounts();
+    }
+
+
+}
